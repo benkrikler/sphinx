6cef104625dcbb9d8f2c6166fd28d09668186abe
Ben Krikler <bek07@ic.ac.uk>
2019-04-21 00:19:43 +0200

    Extend unit test

--- tests/test_ext_viewcode.py
+++ tests/test_ext_viewcode.py
@@ -12,9 +12,7 @@ import re
 
 import pytest
 
-
-@pytest.mark.sphinx(testroot='ext-viewcode')
-def test_viewcode(app, status, warning):
+def check_viewcode_output(app, status, warning):
     app.builder.build_all()
 
     warnings = re.sub(r'\\+', '/', warning.getvalue())
@@ -47,6 +45,19 @@ def test_viewcode(app, status, warning):
             '    <span>&quot;&quot;&quot;</span>\n'
             '<span>    this is Class1</span>\n'
             '<span>    &quot;&quot;&quot;</span>\n') in result
+    return result
+
+
+@pytest.mark.sphinx(testroot='ext-viewcode', confoverrides={"viewcode_linenumbers": True})
+def test_viewcode_linenos(app, status, warning):
+    result = check_viewcode_output(app, status, warning)
+    assert '<table class="highlighttable"><tr><td class="linenos">' in result
+
+
+@pytest.mark.sphinx(testroot='ext-viewcode', confoverrides={"viewcode_linenumbers": False})
+def test_viewcode(app, status, warning):
+    result = check_viewcode_output(app, status, warning)
+    assert 'class="linenos">' not in result
 
 
 @pytest.mark.sphinx(testroot='ext-viewcode', tags=['test_linkcode'])
